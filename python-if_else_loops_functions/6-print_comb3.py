954ef537018f81e458643eca75a1e7e3f4995ef9
FelixBesancon <f.besancon@hotmail.fr>
2026-01-15 14:38:32 +0000

    Add 7-islower.py function

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for number in range(100):
-    if (number == 89):
-        print("{}".format(number))
-        break
-    elif ((number // 10) == (number % 10) or (number // 10) > (number % 10)):
-        continue
-    print("{:02d},".format(number), end=" ")
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d},".format(i, j), end=" ")
