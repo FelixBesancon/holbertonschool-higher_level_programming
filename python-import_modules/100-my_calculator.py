0f46f44102341bffea13ccd1774c8b7ac28643f9
FelixBesancon <f.besancon@hotmail.fr>
2026-01-16 15:27:38 +0000

    Add 100-my calculator.py script

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys, calculator_1
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operations = {
+        "+": calculator_1.add,
+        "-": calculator_1.sub,
+        "*": calculator_1.mul,
+        "/": calculator_1.div
+    }
+    operator = sys.argv[2]
+    if sys.argv[2] not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, operator, b, operations[operator](a, b)))
