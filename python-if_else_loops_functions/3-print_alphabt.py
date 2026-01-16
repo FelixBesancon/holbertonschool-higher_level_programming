fce9f25bc224cdeeea0bea447db971fed96b4726
FelixBesancon <f.besancon@hotmail.fr>
2026-01-16 09:13:29 +0000

    Add 9-print_last_digit.py function

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord("a"), ord("z")+1):
-    if (letter == ord("q") or letter == ord("e")):
+for letter in range(ord('a'), ord('z') + 1):
+    if (chr(letter) in ('q', 'e')):
         continue
     print("{}".format(chr(letter)), end="")
