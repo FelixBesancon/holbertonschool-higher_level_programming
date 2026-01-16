04cca17698612e0ed931e3ac97fb8cde51f798f1
FelixBesancon <f.besancon@hotmail.fr>
2026-01-16 12:35:48 +0000

    add 2-args.py script

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        label = "arguments."
+    elif argc == 1:
+        label = "argument:"
+    else:
+        label = "arguments:"
+    print("{} {}".format(argc, label))
+    for index in range(1, argc + 1):
+       print("{}: {}".format(index, sys.argv[index]))
