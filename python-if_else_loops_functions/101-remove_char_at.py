6800588cbd65b8266ac085d670e71054b268b56b
FelixBesancon <f.besancon@hotmail.fr>
2026-01-16 14:20:51 +0000

    Add 101-remove_char_at.py function

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str_cpy = ""
+    for letter in range (len(str)):
+        if (letter != n):
+            str_cpy += str[letter]
+    return str_cpy
+  
