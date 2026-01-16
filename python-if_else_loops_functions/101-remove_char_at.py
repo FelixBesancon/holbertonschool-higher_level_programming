939fe5b3239cc7e53dc027a17a53fb0e0218fba6
FelixBesancon <f.besancon@hotmail.fr>
2026-01-16 14:21:48 +0000

    Add 101-remove_char_at.py function

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     str_cpy = ""
-    for letter in range (len(str)):
+    for letter in range(len(str)):
         if (letter != n):
             str_cpy += str[letter]
     return str_cpy
-  
