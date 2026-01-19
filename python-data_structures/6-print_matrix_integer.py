ac2c47735c9e75bbd65a8e4cb715646e3841cd95
FelixBesancon <f.besancon@hotmail.fr>
2026-01-19 14:52:15 +0000

    Add 9-max_integer.py function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for line in matrix:
-        for index in range(len(line)):
-            print("{}".format(line[index]), end="")
-            if index != len(line) - 1:
-                print("", end=" ")
+    for row in matrix:
+        for index in range(len(row)):
+            print("{}".format(row[index]), end="")
+            if index != len(row) - 1:
+                print(" ", end="")
         print()
