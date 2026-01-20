00f8c449202beb48367375c9d13c9f12e922db43
FelixBesancon <f.besancon@hotmail.fr>
2026-01-20 13:16:54 +0000

    Add 3-common_elements.py function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for index in range(len(row)):
-            print("{}".format(row[index]), end="")
+        for index, value in enumerate(row):
+            print("{}".format(value), end="")
             if index != len(row) - 1:
                 print(" ", end="")
         print()
