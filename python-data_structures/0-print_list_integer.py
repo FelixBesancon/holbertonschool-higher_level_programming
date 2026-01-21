f055683a24776523aa21d3e4b8f870e27ccb1687
FelixBesancon <f.besancon@hotmail.fr>
2026-01-21 08:00:40 +0000

    Bug fix

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     if my_list is None:
-        return
+        return 0
     for index in my_list:
-        print("{}".format(index))
+        print("{:d}".format(index))
