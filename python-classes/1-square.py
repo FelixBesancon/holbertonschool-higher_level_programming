73d6a11792c22436e4bf561119413a2c785a128a
FelixBesancon <f.besancon@hotmail.fr>
2026-01-26 10:36:27 +0000

    Add 1-2-3-square.py classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines the class Square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+    def __init__(self, size):
+        self.__size = size
