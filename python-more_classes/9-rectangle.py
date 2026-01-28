b56fcad1635570460d8864097e15a2eff378c721
FelixBesancon <f.besancon@hotmail.fr>
2026-01-28 10:29:11 +0000

    bug fix

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        self.__class__.number_of_instances += 1
 
     @property
     def width(self):
@@ -160,5 +160,5 @@ class Rectangle:
         """
         Prints a message when an instance of Rectangle is deleted.
         """
-        Rectangle.number_of_instances -= 1
+        self.__class__.number_of_instances -= 1
         print("Bye rectangle...")
