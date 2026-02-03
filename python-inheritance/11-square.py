50e64eaace5e076090132d998a785c35382b89ba
FelixBesancon <f.besancon@hotmail.fr>
2026-02-03 12:57:13 +0000

    Add 11-square.py class

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines the class Square.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes an instance of Square, with:
+            size.
+        """
+        super().__init__(size, size)
+    
+    def __str__(self):
+        """
+        Returns the following square description.
+        """
+        return "[Square] {}/{}".format(
+            self._Rectangle__width,
+            self._Rectangle__height
+        )
