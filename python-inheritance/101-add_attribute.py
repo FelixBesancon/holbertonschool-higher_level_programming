3a659dfece299bba4d48b2aeff9ec269ca352ead
FelixBesancon <f.besancon@hotmail.fr>
2026-02-04 11:20:22 +0000

    Add docstring

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""Module that provides a function to add attributes to objects."""
+
+
 def add_attribute(obj, attr, value):
+    """Add a new attribute to an object if possible.
+
+    Args:
+        obj: The target object.
+        attr (str): Name of the attribute to add.
+        value: Value of the attribute.
+
+    Raises:
+        TypeError: If the object cannot receive new attributes.
+    """
     if hasattr(obj, "__dict__"):
         setattr(obj, attr, value)
     else:
