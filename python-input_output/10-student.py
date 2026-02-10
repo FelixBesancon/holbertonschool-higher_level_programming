e8e781b9327435d8b9b4c02ab72448fd316c5ba7
FelixBesancon <f.besancon@hotmail.fr>
2026-02-10 08:31:20 +0000

    Change order of public instance attributes

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -2,8 +2,8 @@
 """
 This module provides the class Student, with:
     Public instance attributes:
-        first_name
         last_name
+        first_name
         age
     Public method:
         to_json(self)
@@ -15,12 +15,12 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """
         Initializes a student with:
-            first_name
             last_name
+            first_name
             age
         """
-        self.first_name = first_name
         self.last_name = last_name
+        self.first_name = first_name
         self.age = age
 
     def to_json(self, attrs=None):
