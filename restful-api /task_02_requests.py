11483bba16ab53f464b66e5986c11889fab050f9
FelixBesancon <f.besancon@hotmail.fr>
2026-02-19 08:49:44 +0000

    Trying a new approach for the task

--- restful-api /task_02_requests.py
+++ restful-api /task_02_requests.py
@@ -26,16 +26,19 @@ def fetch_and_save_posts():
     in a csv file.
     """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if response.status_code == requests.codes.ok:
-        post_list = response.json()
+    response_code = response.status_code
+    print("Status Code: {}".format(response_code))
+    if response_code == requests.codes.ok:
         data_list = []
+        post_list = response.json()
         for element in post_list:
             data = {}
             data["id"] = element.get("id")
             data["title"] = element.get("title")
             data["body"] = element.get("body")
             data_list.append(data)
-        with open("posts.csv", "w", newline="", encoding="utf-8") as f:
+        with open("post_list.csv", "w", newline="", encoding="utf-8") as f:
             writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
             writer.writeheader()
-            writer.writerows(data_list)
+            for element in data_list:
+                writer.writerow(element)
