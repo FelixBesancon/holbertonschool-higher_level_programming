a3bef9ffa6ca465e80835838107df00a4923b117
FelixBesancon <f.besancon@hotmail.fr>
2026-02-19 18:00:12 +0000

    Add MyHandler class and do_GET handler in task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 """
+This module provides:
+    MyHandler class, subclass of
+    http.server.BaseHTTPRequestHandler, with:
+        do_GET method.
 """
+
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+    """Handle HTTP GET requests."""
+    def do_GET(self):
+        """Send a simple text response."""
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello, this is a simple API!")
+
+server = HTTPServer(("localhost", 8000), MyHandler)
+server.serve_forever()
