32428682805170ac7aa65ab9821b048df4eeda0c
FelixBesancon <f.besancon@hotmail.fr>
2026-02-19 08:29:29 +0000

    bug fix

--- restful-api /task_02_requests.py
+++ restful-api /task_02_requests.py
@@ -10,34 +10,33 @@ import csv
 
 
 def fetch_and_print_posts():
-    """Fetch posts from an API and print their titles."""
+    """Fetch post_list from an API and print their titles."""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     response_code = response.status_code
     print("Status Code: {}".format(response_code))
     if response_code == requests.codes.ok:
-        posts = response.json()
-        for element in posts:
-            print(element["title"])
+        post_list = response.json()
+        for post in post_list:
+            print(post["title"])
 
 
 def fetch_and_save_posts():
     """
-    Fetch posts from an API and saves their id, title and body
+    Fetch post_list from an API and saves their id, title and body
     in a csv file.
     """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    response_code = response.status_code
-    if response_code == requests.codes.ok:
+    if response.status_code == requests.codes.ok:
         data_list = []
-        posts = response.json()
-        for element in posts:
+        post_list = response.json()
+        for element in post_list:
             data = {}
             data["id"] = element.get("id")
             data["title"] = element.get("title")
             data["body"] = element.get("body")
             data_list.append(data)
-        with open("posts.csv", "w", newline="", encoding="utf-8") as f:
+        with open("post_list.csv", "w", newline="", encoding="utf-8") as f:
             writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
             writer.writeheader()
-            for csv_data in data_list:
-                writer.writerow(csv_data)
+            for element in data_list:
+                writer.writerow(element)
