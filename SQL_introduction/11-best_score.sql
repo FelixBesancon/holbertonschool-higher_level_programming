a86dd34733abbd1c6d54d461775f2feb22140ba1
FelixBesancon <f.besancon@hotmail.fr>
2026-03-03 10:25:01 +0000

    Add tasks 11, 12 and 13 files

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- Tasks 11. Select the best
+-- Script that lists all records with a score >= 10 in the table
+-- second_table of the database hbtn_0c_0 in the MySQL server
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
