93b994147c7b75bbf9f6805be49aa9c7cbd56b22
FelixBesancon <f.besancon@hotmail.fr>
2026-03-03 08:43:03 +0000

    AddSQL_introduction directory, README.md file, task 0 and task 1 files

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- Tasks 0. List databases
+-- Script that lists all databases of the MySQL server
+SHOW DATABASES;
---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Tasks 1. Create a database
+-- Script that creates the database hbtn_0c_0 in the MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
