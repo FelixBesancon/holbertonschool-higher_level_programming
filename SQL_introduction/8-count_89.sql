d4c34db3d82c73a24299c17e4e9abb4b807f1e9e
FelixBesancon <f.besancon@hotmail.fr>
2026-03-03 10:12:12 +0000

    Add tasks 8, 9 and 10 files

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- Tasks 8. Count 89
+-- Script that displays the number of records with id = 89
+-- in the table first_table of the database hbtn_0c_0 in the MySQL server
+SELECT COUNT(id)
+FROM first_table
+WHERE id = 89;
