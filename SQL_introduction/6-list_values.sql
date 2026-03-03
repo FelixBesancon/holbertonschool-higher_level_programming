e6b257e352f02e2b487cc10c4a05e5940b2727db
FelixBesancon <f.besancon@hotmail.fr>
2026-03-03 09:39:49 +0000

    Add tasks 6 and 7 files

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Tasks 6. List all in table
+-- Script that lists all rows of the table first_table
+-- from the database hbtn_0c_0 in the MySQL server
+SELECT * FROM first_table;
---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Tasks 7. First add
+-- Script that inserts a new row in the table first_table (database hbtn_0c_0)
+-- in the MySQL server
+INSERT INTO first_table SET id = 89, name = 'Best School';
