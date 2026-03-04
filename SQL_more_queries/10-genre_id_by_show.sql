b99775745ff4537e703692005513acb45fb41d71
FelixBesancon <f.besancon@hotmail.fr>
2026-03-04 13:28:37 +0000

    Add tasks 9 and 10 files

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- Tasks 10. Genre ID by show
+-- Script that lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked
+SELECT s.title, sg.genre_id
+FROM tv_shows AS s
+INNER JOIN tv_show_genres AS sg
+ON s.id = sg.show_id
+ORDER BY s.title ASC, sg.genre_id ASC;
