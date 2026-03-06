a09bf2276dbd68ba0bd9bae21464714f57f62b73
FelixBesancon <f.besancon@hotmail.fr>
2026-03-06 15:27:27 +0000

    Add task 14 files

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines the City class and its mapping to the MySQL table states.
+"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    Represents a city stored in the cities table.
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
