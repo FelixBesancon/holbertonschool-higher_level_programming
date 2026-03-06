b97d2d9bde9b59edf63d0e2bd118a1c664fadf9a
FelixBesancon <f.besancon@hotmail.fr>
2026-03-06 10:42:01 +0000

    Add task 5 file

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        charset="utf8"
+        )
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states "
+        "ON cities.state_id = states.id "
+        "WHERE BINARY states.name = %s "
+        "ORDER BY cities.id ASC ",
+        (sys.argv[4],)
+        )
+
+    rows = cur.fetchall()
+    cities = [row[0] for row in rows]
+    print(", ".join(cities))
+
+    cur.close()
+    conn.close()
