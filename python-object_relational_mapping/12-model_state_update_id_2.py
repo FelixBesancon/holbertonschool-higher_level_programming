6070591116c3bf0a65a85ce99156384ffbc67c9f
FelixBesancon <f.besancon@hotmail.fr>
2026-03-06 14:44:45 +0000

    Add task 12 file

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Updates the name of the State with id=2 to 'New Mexico'
+in the database using SQLAlchemy.
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+            ),
+        pool_pre_ping=True
+        )
+
+    session = Session(engine)
+
+    state = (
+        session.query(State)
+        .filter_by(id=2)
+        .first()
+        )
+
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
