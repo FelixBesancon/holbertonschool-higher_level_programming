63eb8d7dcf6e7652778d6adafd21b0f52df0086c
FelixBesancon <f.besancon@hotmail.fr>
2026-03-06 08:46:37 +0000

    Add README.md and task 0 files

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Results are sorted in ascending order by id.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        charset="utf8"
+    )
+    cur = conn.cursor()
+
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
