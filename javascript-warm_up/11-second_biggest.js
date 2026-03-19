ea5546c7e3910af848bd10af23618ada9eaf6c0f
FelixBesancon <f.besancon@hotmail.fr>
2026-03-19 18:11:28 +0000

    Task 11. Second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
-function findMax (sortedList) {
-  if (sortedList.length <= 1) {
-    return 0;
-  }
-  return sortedList[1];
+const sortedList = process.argv.slice(2).map(Number).sort((a, b) => b - a);
+let secondMax = 0;
+if (sortedList.length > 1) {
+  secondMax = sortedList[1];
 }
-
-const sortedList = process.argv.slice(2).sort((a, b) => b - a);
-console.log(findMax(sortedList));
+console.log(secondMax);
