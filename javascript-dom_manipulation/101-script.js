3ecb6c92463997aaa56fe298c871928cf7ff1a1f
FelixBesancon <f.besancon@hotmail.fr>
2026-03-23 15:53:14 +0000

    Task 10. Say hello to everybody!

--- javascript-dom_manipulation/101-script.js
+++ javascript-dom_manipulation/101-script.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const hello = document.querySelector('#hello');
   const languageCode = document.querySelector('#language_code');
   const btnTranslate = document.querySelector('#btn_translate');
-  btnTranslate.addEventListener('click', ()=>{
+
+  btnTranslate.addEventListener('click', () => {
     const language = languageCode.value;
+
     fetch('https://hellosalut.stefanbohacek.com/?lang=' + language)
-    .then(response => response.json())
-    .then(data => {
-      hello.textContent = data.hello;
-    });
-  })
+      .then(response => response.json())
+      .then(data => {
+        hello.textContent = data.hello;
+      });
+  });
 });
