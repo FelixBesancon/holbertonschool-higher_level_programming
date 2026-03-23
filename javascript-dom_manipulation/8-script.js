691431a0e8a90d34f5a8550e7ed60d24afb8f835
FelixBesancon <f.besancon@hotmail.fr>
2026-03-23 15:30:02 +0100

    Task 8. Say Hello!

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,6 +1,8 @@
-const hello = document.querySelector('#hello');
-fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
-  .then(response => response.json())
-  .then(data => {
-    hello.textContent = data.hello;
+document.addEventListener('DOMContentLoaded', () => {
+  const hello = document.querySelector('#hello');
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      hello.textContent = data.hello;
+    });
   });
