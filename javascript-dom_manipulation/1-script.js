d8e92b5e4a05c31c1ca3cca633aa898e598c9813
FelixBesancon <f.besancon@hotmail.fr>
2026-03-23 09:56:16 +0000

    Task 1. Click and turn red

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,4 +1,4 @@
 const header = document.querySelector('#red_header');
-header.addEventListener('click', function() {
+header.addEventListener('click', function () {
   header.style.color = '#FF0000';
 });
